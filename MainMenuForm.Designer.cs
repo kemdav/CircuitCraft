e04146dee06cf3f41bab5ffe548c40f101d239d5
LAB65_USER ON WS27 <L65X12W27@citulab.net>
2025-03-20 12:45:28 +0800

    - Removed background worker

--- MainMenuForm.Designer.cs
+++ MainMenuForm.Designer.cs
@@ -36,7 +36,6 @@
             leaderboardButton = new MaterialSkin.Controls.MaterialButton();
             tutorialButton = new MaterialSkin.Controls.MaterialButton();
             playButton = new MaterialSkin.Controls.MaterialButton();
-            backgroundWorker = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)mainMenuBackgroundMedia).BeginInit();
             SuspendLayout();
             // 
@@ -222,6 +221,5 @@
         private MaterialSkin.Controls.MaterialButton leaderboardButton;
         private MaterialSkin.Controls.MaterialButton tutorialButton;
         private MaterialSkin.Controls.MaterialButton playButton;
-        private System.ComponentModel.BackgroundWorker backgroundWorker;
     }
 }
