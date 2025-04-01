812168b7dc760664ae11f6d267830286e805ebfd
InsaneCoder <kemdavid10@gmail.com>
2025-04-01 22:14:51 +0800

    Initial Files for the new Game System

---
+++ CircuitBlock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitCraft
+{
+    public enum CircuitBlockType
+    {
+        Series,
+        Parallel
+    }
+    public class CircuitBlock
+    {
+        public List<CircuitElement> circuitElements = new List<CircuitElement>();
+        public CircuitBlockType circuitBlockType { get; set; }
+        public int maximumNumber { get; set; } = 3;
+
+        public CircuitBlock(CircuitBlockType circuitBlockType)
+        {
+            this.circuitBlockType = circuitBlockType;
+        }
+    }
+}
---
+++ MainGame.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace CircuitCraft
+{
+    partial class MainGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainGame));
+            hopeForm1 = new ReaLTaiizor.Forms.HopeForm();
+            SuspendLayout();
+            // 
+            // hopeForm1
+            // 
+            hopeForm1.ControlBoxColorH = Color.FromArgb(228, 231, 237);
+            hopeForm1.ControlBoxColorHC = Color.FromArgb(245, 108, 108);
+            hopeForm1.ControlBoxColorN = Color.White;
+            hopeForm1.Dock = DockStyle.Top;
+            hopeForm1.Font = new Font("Segoe UI", 12F);
+            hopeForm1.ForeColor = Color.FromArgb(242, 246, 252);
+            hopeForm1.Image = (Image)resources.GetObject("hopeForm1.Image");
+            hopeForm1.Location = new Point(0, 0);
+            hopeForm1.Name = "hopeForm1";
+            hopeForm1.Size = new Size(1264, 40);
+            hopeForm1.TabIndex = 0;
+            hopeForm1.Text = "hopeForm1";
+            hopeForm1.ThemeColor = Color.FromArgb(92, 173, 255);
+            // 
+            // MainGame
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1264, 681);
+            Controls.Add(hopeForm1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1920, 1032);
+            MinimumSize = new Size(190, 40);
+            Name = "MainGame";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "MainGame";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Forms.HopeForm hopeForm1;
+    }
+}
